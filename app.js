e734dce6f7bd0cadc27316b8dcc98986ac9b429e
AmineBoutachdat7 <61319675+AmineBoutachdat7@users.noreply.github.com>
2020-06-26 19:55:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,11 +21,10 @@ dotenv.config();
 // Configure Passport to use Auth0
 var strategy = new Auth0Strategy(
   {
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://127.0.0.1:3000/callback'
+    domain: "progproj1-6.eu.auth0.com",
+    clientID: "LZzJCAaT78s1CfWEbJpd8erUrSafJSfW",
+    clientSecret: "A1YAlZcDhJbRZyapxtLUtIv9zA4wedcl7xu1OFG7N57053fD1Y1P4WjS6X8VCXlj",
+    callbackURL: "http://localhost:3000/callback"
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
